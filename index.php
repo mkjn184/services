545249c817303bbd25680e8beffebd574f498dfb
Mohamed Khaled Bader Ajour <m0ham3dajj@gmail.com>
2017-09-09 11:00:32 +0300

    first com

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php get_header(); ?>
+<?php get_header(); //get header ?>
     <div class="container">
         <div class="row">
             <!-- Carousel -->
--- page.php
+++ page.php
@@ -10,7 +10,7 @@
  * @subpackage Twenty_Sixteen
  * @since Twenty Sixteen 1.0
  */
-
+//mohamed ajour
 get_header(); ?>
 
 <div id="primary" class="content-area">
